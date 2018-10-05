9cf0c7e1c2fe2e37d0785ced785597140cb2c3d3
hmongkey12 <khammadaolee@gmail.com>
2018-10-05 14:55:41 -0700

    Term 1 SumOfTwo Class

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author caillou
+ *
+ */
+module SumOfTwo {
+}
---
+++ src/sumoftwo/SumOfTwo.java
@@ -0,0 +1,13 @@
+package sumoftwo;
+
+public class SumOfTwo {
+	
+	public static int sum_of_two( int num1, int num2) {
+		return (num1 + num2);
+	}
+	public static void main(String[] args) {
+		sum_of_two(1, 2);
+		return;
+	}
+	
+}
